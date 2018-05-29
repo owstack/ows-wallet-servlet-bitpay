372b22262ec93f878762e081485ec1076516e9f9
ajp8164 <andyphillipson@gmail.com>
2018-05-29 18:36:54 -0400

    Add support for commit hash, fix build script

--- util/build.js
+++ util/build.js
@@ -4,31 +4,35 @@
 
 var fs = require('fs-extra');
 var execSync = require('child_process').execSync;
+var shell = require('shelljs');
 
 // This script runs from the project root directory via Grunt.
 
 console.log("Starting build");
 
-console.log('>> [1/3] Exporting images from resources sketch file');
+console.log('>> [1/4] Exporting images from resources sketch file');
 exportResourcesSketch();
 
-console.log('>> [2/3] Updating package.json');
+console.log('>> [2/4] Updating package.json');
 updatePackageJson();
 
-console.log('>> [3/3] Building index.html');
+console.log('>> [3/4] Updating plugin.json');
+updatePluginJson();
+
+console.log('>> [4/4] Building index.html');
 createIndexHtml();
 
 console.log("Build complete");
 
 //////////////
 
-// [1/3] Generate image resources from sketch file.
+// [1/4] Generate image resources from sketch file.
 //
 function exportResourcesSketch() {
   execSync('sh ./generate.sh', { cwd: './util/resources', stdio: [0,1,2] });
 };
 
-// [2/3] Update fields in package.json from plugin.json.
+// [2/4] Update fields in package.json from plugin.json.
 //
 function updatePackageJson() {
   // Read and parse content of each file.
@@ -46,7 +50,21 @@ function updatePackageJson() {
   fs.writeFileSync('./package.json', pkg, 'utf8');
 };
 
-// [3/3] Create the index.html file by substituting values from json config files.
+// [3/4] Update fields in plugin.json.
+//
+function updatePluginJson() {
+  // Read and parse content.
+  var plugin = getPlugin();
+
+  // Create assignments in plugin.json
+  plugin.header.commitHash = getCommitHash();
+
+  // Write the result.
+  plugin = JSON.stringify(plugin, null, 2);
+  fs.writeFileSync('./plugin.json', plugin, 'utf8');
+};
+
+// [4/4] Create the index.html file by substituting values from json config files.
 //
 function createIndexHtml() {
   var pkg = getPackage();
@@ -95,4 +113,14 @@ function getPackage() {
 function getPlugin() {
   var plugin = fs.readFileSync('./plugin.json', 'utf8');
   return JSON.parse(plugin, 'utf8');
+};
+
+// Get the commit hash for this plugin.
+//
+function getCommitHash() {
+  // Exec git command to get the hash of the current commit
+  var hash = shell.exec('git rev-parse HEAD', {
+    silent: true
+  }).output.trim().substr(0, 7);
+  return hash;
 };
